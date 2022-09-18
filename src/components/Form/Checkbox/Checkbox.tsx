ba9bead15c7c945ded3c2955a0060abdc7bb2492
Ahmed Eid <aeid@hypermedialabs.com>
2022-09-18 22:48:21 +0200

    fix to checkbox

---
+++ src/components/Form/Checkbox/Checkbox.tsx
@@ -0,0 +1,45 @@
+import { Checkbox as CharkaCheckbox } from '@chakra-ui/checkbox';
+import { Text } from '@chakra-ui/layout';
+import { Field, FieldProps } from 'formik';
+import React from 'react';
+
+interface CheckboxProps {
+  name: string;
+  children?: React.ReactNode;
+  checked?: boolean;
+  className?: string;
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({
+  name,
+  children,
+  checked,
+  className,
+}) => {
+  return (
+    <Field name={name}>
+      {({ field, form }: FieldProps) => (
+        <CharkaCheckbox
+          defaultChecked={checked}
+          iconColor='#21545F'
+          colorScheme='white'
+          style={{
+            borderColor: '#21545F',
+          }}
+          id={name}
+          name={name}
+          onChange={(e) => {
+            form.setFieldValue(name, e.target.checked);
+          }}
+          className={className}
+        >
+          <Text fontSize='sm' textAlign='left'>
+            {children}
+          </Text>
+        </CharkaCheckbox>
+      )}
+    </Field>
+  );
+};
+
+export default Checkbox;
