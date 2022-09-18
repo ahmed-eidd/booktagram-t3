97b46e9b1dd173b66dbe4ccfbfd540b37f830196
Ahmed Eid <aeid@hypermedialabs.com>
2022-09-18 02:48:28 +0200

    fix select

--- src/components/Form/Select/Select.tsx
+++ src/components/Form/Select/Select.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactSelect, { StylesConfig } from 'react-select';
+import ReactSelect from 'react-select';
+import {StylesProps, StylesConfig} from 'react-select/dist/declarations/src/styles'
 import { FieldHookConfig, useField } from 'formik';
 import { colors } from '../../../styles/abstract/colors';
 import FormControl from '../FormControl/FormControl';
@@ -16,6 +17,7 @@ interface SelectProps {
   placeholder?: string;
   options: { label: string; value: string }[];
   className?: string;
+  style: React.CSSProperties;
 }
 
 const Select: React.FC<SelectProps & FieldHookConfig<string>> = ({
@@ -24,6 +26,7 @@ const Select: React.FC<SelectProps & FieldHookConfig<string>> = ({
   placeholder,
   options,
   className,
+  style,
   ...props
 }) => {
   const [field, { error, touched }, helpers] = useField(props);
@@ -34,6 +37,11 @@ const Select: React.FC<SelectProps & FieldHookConfig<string>> = ({
   ) => {
     return options ? options.find((option) => option?.value === value) : '';
   };
+
+  // const customControlStyles = {
+  //   backgroundColor: grey,
+  //   color: secondary,
+  // };
 
   const { grey, secondary } = colors;
   // styles
@@ -47,12 +55,12 @@ const Select: React.FC<SelectProps & FieldHookConfig<string>> = ({
       },
     }),
     dropdownIndicator: (styles) => ({ ...styles, color: grey }),
-    indicatorSeparator: (styles) => ({ display: 'none' }),
+    // indicatorSeparator: (styles) => ({ display: 'none' }),
     menu: (styles) => ({ ...styles, backgroundColor: secondary }),
     option: (styles, { isSelected }) => ({
       ...styles,
-      backgroundColor: isSelected && grey,
-      color: isSelected && secondary,
+      // backgroundColor: isSelected && grey,
+      // color: isSelected && secondary,
       ':active': {
         backgroundColor: grey,
         color: secondary,
@@ -75,23 +83,23 @@ const Select: React.FC<SelectProps & FieldHookConfig<string>> = ({
       label={label}
       labelStyle={labelStyle}
       field={field}
+      style={style}
     >
       <ReactSelect
         {...field}
         id={field.name}
         placeholder={placeholder}
-        {...props}
-        styles={colourStyles}
+        // styles={colourStyles}
         onChange={(value) => {
           if (value === null) {
             setValue('');
             return;
           }
-          setValue(value?.value);
+          setValue(value.toString());
         }}
         value={defaultValue(options, field.value)}
         options={options}
-      ></ReactSelect>
+      />
     </FormControl>
   );
 };
